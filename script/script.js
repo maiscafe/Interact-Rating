71fdc269e52d34e268e61964cc1efb3a42f80d35
maiscafe <adriano_tomaban@hotmail.com>
2022-05-11 13:23:24 -0300

    iniciando segunda janela

---
+++ script/script.js
@@ -0,0 +1,9 @@
+/* Hidden first page and show second page */
+
+var btnSubmit = document.querySelector("#btnSubmit")
+
+btnSubmit.addEventListener("click", function (){
+    var firstPage = document.querySelector ("#first-page")
+    firstPage.classList.add("hidden")
+    
+});
