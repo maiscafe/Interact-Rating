c0748e7ed56ebecf47de94beeaf7d01c57ba51c7
maiscafe <adriano_tomaban@hotmail.com>
2022-05-11 15:17:31 -0300

    two pages working

--- script/script.js
+++ script/script.js
@@ -1,9 +1,29 @@
 /* Hidden first page and show second page */
-
+var firstPage = document.querySelector ("#first-page");
+var secondPage = document.querySelector ("#second-page");
+var alertSpan = document.querySelector ("#alert-span");
 var btnSubmit = document.querySelector("#btnSubmit")
+firstPage.style.display = "flex";
+secondPage.style.display = "none";
+alertSpan.style.display = "none";
 
-btnSubmit.addEventListener("click", function (){
-    var firstPage = document.querySelector ("#first-page")
-    firstPage.classList.add("hidden")
-    
-});
+
+    btnSubmit.addEventListener("click", showSecondPage);
+    secondPage.addEventListener("click",showAlert);
+
+
+function showFirstPage(){
+    firstPage.style.display = "flex";
+    secondPage.style.display = "none";
+    thirdPage.style.display = "none";
+}
+
+function showSecondPage(){
+    firstPage.style.display = "none";
+    secondPage.style.display = "flex";
+    thirdPage.style.display = "none"
+}
+
+function showAlert(){
+    alert("Thank you for your submission!");
+}
